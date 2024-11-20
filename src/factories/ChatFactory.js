ec0173df4200bd30fe31024b873dd2ef5409d9e8
K H Soh <hju37823@outlook.com>
2024-11-20 09:36:30 +0800

    Official pull request to fix isGroup issue from https://github.com/pedroslopez/whatsapp-web.js/pull/3380

--- src/factories/ChatFactory.js
+++ src/factories/ChatFactory.js
@@ -5,8 +5,6 @@ const GroupChat = require('../structures/GroupChat');
 
 class ChatFactory {
     static create(client, data) {
-        // Workaround lack of isGroup property in newest WhatsApp Web
-        data.isGroup = data.id && data.id.server == "g.us";
         if (data.isGroup) {
             return new GroupChat(client, data);
         }
