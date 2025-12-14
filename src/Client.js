1072b9eb682e849efe55f3d8f0a04577be74e144
K H Soh <hju37823@outlook.com>
2025-12-14 22:08:03 +0800

    Additional changes to get the actual phone number

--- src/Client.js
+++ src/Client.js
@@ -1397,8 +1397,8 @@ class Client extends EventEmitter {
                 content instanceof Buttons,
                 content instanceof List,
                 Array.isArray(content) &&
-                    content.length > 0 &&
-                    content[0] instanceof Contact,
+                content.length > 0 &&
+                content[0] instanceof Contact,
             ].includes(true)
         ) {
             console.warn(
@@ -1419,8 +1419,8 @@ class Client extends EventEmitter {
                 content instanceof Buttons,
                 content instanceof List,
                 Array.isArray(content) &&
-                    content.length > 0 &&
-                    content[0] instanceof Contact,
+                content.length > 0 &&
+                content[0] instanceof Contact,
             ].includes(true)
         ) {
             console.warn(
@@ -1802,10 +1802,10 @@ class Client extends EventEmitter {
             return !pinnedMsgs.length
                 ? []
                 : await Promise.all(
-                      pinnedMsgs.map((msg) =>
-                          window.WWebJS.getMessageModel(msg),
-                      ),
-                  );
+                    pinnedMsgs.map((msg) =>
+                        window.WWebJS.getMessageModel(msg),
+                    ),
+                );
         }, chatId);
 
         return pinnedMsgs.map((msg) => new Message(this, msg));
@@ -2107,9 +2107,9 @@ class Client extends EventEmitter {
                         .Chat.find(chatId));
                 action === 'MUTE'
                     ? await chat.mute.mute({
-                          expiration: unmuteDateTs,
-                          sendDevice: true,
-                      })
+                        expiration: unmuteDateTs,
+                        sendDevice: true,
+                    })
                     : await chat.mute.unmute({ sendDevice: true });
                 return {
                     isMuted: chat.mute.expiration !== 0,
@@ -2393,9 +2393,9 @@ class Client extends EventEmitter {
                                 window
                                     .require('WAWebCollections')
                                     .Chat.get(participant.wid) ||
-                                    (await window
-                                        .require('WAWebCollections')
-                                        .Chat.find(participant.wid)),
+                                (await window
+                                    .require('WAWebCollections')
+                                    .Chat.find(participant.wid)),
                                 createGroupResult.wid._serialized,
                                 createGroupResult.subject,
                                 participant.invite_code,
@@ -2660,11 +2660,11 @@ class Client extends EventEmitter {
 
                 countryCodes =
                     countryCodes.length === 1 &&
-                    countryCodes[0] === currentRegion
+                        countryCodes[0] === currentRegion
                         ? countryCodes
                         : countryCodes.filter((code) =>
-                              Object.keys(countryCodesIso).includes(code),
-                          );
+                            Object.keys(countryCodesIso).includes(code),
+                        );
 
                 const viewTypeMapping = {
                     0: 'RECOMMENDED',
@@ -2703,12 +2703,12 @@ class Client extends EventEmitter {
 
                 return channels
                     ? await Promise.all(
-                          channels.map((channel) =>
-                              window.WWebJS.getChatModel(channel, {
-                                  isChannel: true,
-                              }),
-                          ),
-                      )
+                        channels.map((channel) =>
+                            window.WWebJS.getChatModel(channel, {
+                                isChannel: true,
+                            }),
+                        ),
+                    )
                     : [];
             },
             searchOptions,
