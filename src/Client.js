01eb59513e727e5130d562e6ed183442ceb92ae8
K H Soh <hju37823@outlook.com>
2026-03-15 15:28:58 +0800

    Changes to convert to whatsapp-web.js editor config style

--- src/Client.js
+++ src/Client.js
@@ -723,8 +723,12 @@ class Client extends EventEmitter {
                     /** @type {GroupNotification} object does not provide enough information about this event, so a @type {Message} object is used. */
                     const message = new Message(this, msg);
 
-                    const newId = isParticipant ? msg.recipients[0] : msg._data.templateParams[1];
-                    const oldId = isParticipant ? msg.author : msg._data.templateParams[0];
+                    const newId = isParticipant
+                        ? msg.recipients[0]
+                        : msg._data.templateParams[1];
+                    const oldId = isParticipant
+                        ? msg.author
+                        : msg._data.templateParams[0];
 
                     /**
                      * Emitted when a contact or a group participant changes their phone number.
@@ -1323,8 +1327,8 @@ class Client extends EventEmitter {
                 content instanceof Buttons,
                 content instanceof List,
                 Array.isArray(content) &&
-                content.length > 0 &&
-                content[0] instanceof Contact,
+                    content.length > 0 &&
+                    content[0] instanceof Contact,
             ].includes(true)
         ) {
             console.warn(
@@ -1345,8 +1349,8 @@ class Client extends EventEmitter {
                 content instanceof Buttons,
                 content instanceof List,
                 Array.isArray(content) &&
-                content.length > 0 &&
-                content[0] instanceof Contact,
+                    content.length > 0 &&
+                    content[0] instanceof Contact,
             ].includes(true)
         ) {
             console.warn(
@@ -1701,10 +1705,10 @@ class Client extends EventEmitter {
             return !pinnedMsgs.length
                 ? []
                 : await Promise.all(
-                    pinnedMsgs.map((msg) =>
-                        window.WWebJS.getMessageModel(msg),
-                    ),
-                );
+                      pinnedMsgs.map((msg) =>
+                          window.WWebJS.getMessageModel(msg),
+                      ),
+                  );
         }, chatId);
 
         return pinnedMsgs.map((msg) => new Message(this, msg));
@@ -2006,9 +2010,9 @@ class Client extends EventEmitter {
                         .Chat.find(chatId));
                 action === 'MUTE'
                     ? await chat.mute.mute({
-                        expiration: unmuteDateTs,
-                        sendDevice: true,
-                    })
+                          expiration: unmuteDateTs,
+                          sendDevice: true,
+                      })
                     : await chat.mute.unmute({ sendDevice: true });
                 return {
                     isMuted: chat.mute.expiration !== 0,
@@ -2292,9 +2296,9 @@ class Client extends EventEmitter {
                                 window
                                     .require('WAWebCollections')
                                     .Chat.get(participant.wid) ||
-                                (await window
-                                    .require('WAWebCollections')
-                                    .Chat.find(participant.wid)),
+                                    (await window
+                                        .require('WAWebCollections')
+                                        .Chat.find(participant.wid)),
                                 createGroupResult.wid._serialized,
                                 createGroupResult.subject,
                                 participant.invite_code,
@@ -2559,11 +2563,11 @@ class Client extends EventEmitter {
 
                 countryCodes =
                     countryCodes.length === 1 &&
-                        countryCodes[0] === currentRegion
+                    countryCodes[0] === currentRegion
                         ? countryCodes
                         : countryCodes.filter((code) =>
-                            Object.keys(countryCodesIso).includes(code),
-                        );
+                              Object.keys(countryCodesIso).includes(code),
+                          );
 
                 const viewTypeMapping = {
                     0: 'RECOMMENDED',
@@ -2602,12 +2606,12 @@ class Client extends EventEmitter {
 
                 return channels
                     ? await Promise.all(
-                        channels.map((channel) =>
-                            window.WWebJS.getChatModel(channel, {
-                                isChannel: true,
-                            }),
-                        ),
-                    )
+                          channels.map((channel) =>
+                              window.WWebJS.getChatModel(channel, {
+                                  isChannel: true,
+                              }),
+                          ),
+                      )
                     : [];
             },
             searchOptions,
